2b362af5f7c375fa446d89708f22fb6254852361
daniel-wtd <daniel@while-true-do.io>
2019-04-29 10:51:39 +0200

    fix: blank lines

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,6 +5,7 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+
 # Only some dummy test, since testing dynamic packages is aweful
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
